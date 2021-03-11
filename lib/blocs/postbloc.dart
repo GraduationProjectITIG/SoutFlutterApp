f8b74eb5b5bfc1443a39cbe20c9c049c10d0eca6
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-03-11 13:01:07 +0200

    adding user functions and postbloc

--- lib/blocs/postbloc.dart
+++ lib/blocs/postbloc.dart
@@ -1,8 +1,31 @@
+import 'package:rxdart/rxdart.dart';
 import 'package:sout/base_bloc.dart';
+import 'package:sout/models/models.dart';
 
 class PostBloc extends BaseBloc {
+static final PostModel post = PostModel();
+static List<PostModel> list = [];
+
+  final BehaviorSubject<PostModel> _postController =
+      BehaviorSubject<PostModel>.seeded(post);
+
+  final BehaviorSubject<List<PostModel>> _postListController =
+      BehaviorSubject<List<PostModel>>.seeded(list);
+
+  Stream<PostModel> get postStream => _postController.stream;
+
+Stream<PostModel> get postListStream => _postListController.stream;
+
+  // Future getAllPosts() async {
+  //   await  post.getAllPosts().then((value){
+  //     _postListController.add(value);
+  //   });
+  // }
+
   @override
   void dispose() {
     // TODO: implement dispose
+    _postController.drain();
+    _postController.close();
   }
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,25 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class UserModel {
-  String id,firstName,lastName, email,password,confirmPassword,favColor,favMode,mobile,gender,picURL,coverPicURL,birthDate;
-  bool blocked,privateAcc,islogin = false;
+  String id,
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      favColor,
+      favMode,
+      mobile,
+      gender,
+      picURL,
+      coverPicURL,
+      birthDate;
+  bool blocked, privateAcc, islogin = false;
   FirebaseAuth _auth = FirebaseAuth.instance;
-  UserModel({this.id,this.firstName,this.lastName, this.email,this.password,this.confirmPassword,this.favColor,this.favMode,this.mobile,this.gender,this.picURL,this.coverPicURL,this.birthDate,this.blocked,this.privateAcc}) {}
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  UserModel(
+      {this.id,
+      this.firstName,
+      this.lastName,
+      this.email,
+      this.password,
+      this.confirmPassword,
+      this.favColor,
+      this.favMode,
+      this.mobile,
+      this.gender,
+      this.picURL,
+      this.coverPicURL,
+      this.birthDate,
+      this.blocked,
+      this.privateAcc});
 
-  // Future<UserModel> login(email, password) async {
-  //   UserModel usr = UserModel();
-  //   await _auth
-  //       .signInWithEmailAndPassword(email: email, password: password)
-  //       .then((value) {
-  //         //TODO add user values.
-  //       });
-  //   return usr;
-  // }
+  Map<String, dynamic> toJson() {
+    return {
+      //TODO continue
+      'id': id,
+    };
+  }
 
-  // Future<List<UserModel>> getUsers() async {
-  //   List<UserModel> list = [];
-  //   //TODO add firestore method
-  //   return list;
-  // }
+  factory UserModel.fromDocument(DocumentSnapshot doc) {
+    return UserModel(
+      //TODO continue
+      id: doc.data()["id"],
+    );
+  }
 
+  Future<UserModel> login(email, password) async {
+    UserModel usr = UserModel();
+    await _auth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) async {
+      if (value.user.uid != null) {
+        await _firestore
+            .collection('Users')
+            .doc(value.user.uid)
+            .get()
+            .then((user) {
+          //TODO continue
+        });
+      } else {
+        Fluttertoast.showToast(
+          msg:
+              'This user is not registered or the password or the email is wrong.',
+          backgroundColor: Colors.red,
+          timeInSecForIosWeb: 10,
+        );
+      }
+    });
+    return usr;
+  }
+
+  Future<List<UserModel>> getUsers() async {
+    List<UserModel> list = [];
+    //TODO add firestore method
+    return list;
+  }
 }
