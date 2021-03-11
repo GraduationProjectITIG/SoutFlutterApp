43e0c461e465ad39fa2ae44a045f0fdc85357cb8
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-03-11 11:22:14 +0200

    adding some func in user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,10 +1,25 @@
-class UserModel {
-  String firstName, email;
-  bool islogin = false;
-  UserModel({this.firstName, this.email}) {}
+import 'package:firebase_auth/firebase_auth.dart';
 
-  Future<UserModel> login(email, password) async {
-    this.islogin = true;
-    return UserModel();
-  }
+class UserModel {
+  String id,firstName,lastName, email,password,confirmPassword,favColor,favMode,mobile,gender,picURL,coverPicURL,birthDate;
+  bool blocked,privateAcc,islogin = false;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  UserModel({this.id,this.firstName,this.lastName, this.email,this.password,this.confirmPassword,this.favColor,this.favMode,this.mobile,this.gender,this.picURL,this.coverPicURL,this.birthDate,this.blocked,this.privateAcc}) {}
+
+  // Future<UserModel> login(email, password) async {
+  //   UserModel usr = UserModel();
+  //   await _auth
+  //       .signInWithEmailAndPassword(email: email, password: password)
+  //       .then((value) {
+  //         //TODO add user values.
+  //       });
+  //   return usr;
+  // }
+
+  // Future<List<UserModel>> getUsers() async {
+  //   List<UserModel> list = [];
+  //   //TODO add firestore method
+  //   return list;
+  // }
+
 }
