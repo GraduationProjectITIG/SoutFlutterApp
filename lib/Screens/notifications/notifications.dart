3438533408c1e187724fd2d3ba8c45f1b7a46d3c
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-03-13 12:46:06 +0200

    finishing notifications and adding assets

--- lib/Screens/notifications/notifications.dart
+++ lib/Screens/notifications/notifications.dart
@@ -17,8 +17,20 @@ class Notifications extends StatefulWidget {
 class _NotificationsState extends State<Notifications> {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List list = [];
+  AppBar appBar = AppBar(
+    title: Text(
+      'Notifications',
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    backgroundColor: Color(0xff292b2c),
+  );
+
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return StreamBuilder<QuerySnapshot>(
         stream: _firestore
             .collection("Users")
@@ -28,19 +40,66 @@ class _NotificationsState extends State<Notifications> {
         builder: (context, snapshot) {
           if (snapshot.data != null) {
             for (var item in snapshot.data.docs) {
-              //TODO continue
-              list.add(ListTile());
+              if (item['seen'] == false)
+                list.add(ListTile(
+                  leading: Container(
+                    child: item['maker']['picURL'] != null &&
+                            item['maker']['picURL'].toString() !=
+                                "./../../../../assets/avatar.png" &&
+                            item['maker']['picURL'].toString() !=
+                                "./../../../../assets/avatar2.png"
+                        ? Image.network(
+                            item['maker']['picURL'].toString(),
+                          )
+                        : item['maker']['picURL'].toString() !=
+                                "./../../../../assets/avatar2.png"
+                            ? Image.asset(
+                                "assets/images/avatar.png",
+                              )
+                            : Image.asset(
+                                "assets/images/avatar2.png",
+                              ),
+                  ),
+                  title: Column(
+                    children: [
+                      Text(
+                        item['description'].toString(),
+                      ),
+                      Text(
+                        DateTime.parse(item['date'].toString())
+                            .toLocal()
+                            .toString(),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    _firestore
+                        .collection("Users")
+                        .doc(sL<UserBloc>().user.id)
+                        .collection('notifications')
+                        .doc(item.id)
+                        .update({"seen": true});
+                  },
+                ));
             }
           }
           return Scaffold(
             drawer: buildDrawer(context),
-                      body: Container(
-              child: ListView.builder(
-                  physics: iosScrollPhysics(),
-                  itemCount: list.length,
-                  itemBuilder: (context, index) {
-                    return list[index];
-                  }),
+            appBar: this.appBar,
+            body: Container(
+              width: mediaQuery.size.width,
+              height: mediaQuery.size.height -
+                  (mediaQuery.padding.top + appBar.toolbarHeight),
+              child: snapshot.data != null
+                  ? ListView.builder(
+                      physics: iosScrollPhysics(),
+                      itemCount: list.length,
+                      itemBuilder: (context, index) {
+                        return list[index];
+                      })
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    ),
             ),
           );
         });
